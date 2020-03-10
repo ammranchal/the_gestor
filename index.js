3cfbae763dbb67afa0728f78df2418b92552eab1
ammranchal <ammranchal@ucm.es>
2020-03-10 22:01:15 +0100

    First change to try deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Heroku CI!');
 });
 
 app.listen(port, function () {

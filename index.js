d5556af2500e0bed2dead081fb7683530bef34c6
ammranchal <ammranchal@ucm.es>
2020-03-10 22:19:19 +0100

    Config view

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/view/index.html");
+});
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/view/index.html');
-});
-
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function (err) {
+    if (err) {
+        console.log('Problemas al conectarse:', err);
+    }
 });
